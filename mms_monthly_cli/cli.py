8b8d7dbd14ac9fd4a1a79e71cfb18c7228a02d41
prakaa <abiprakash007@gmail.com>
2023-07-11 20:02:56 +1000

    migrate CLI to typer

--- mms_monthly_cli/cli.py
+++ mms_monthly_cli/cli.py
@@ -1,78 +1,56 @@
-import argparse
-import sys
-from pprint import pprint
-from typing import Union
+from pathlib import Path
 
-from mms_monthly import (
+import typer
+from rich import print
+from typing_extensions import Annotated
+
+from .mms_monthly import (
     get_and_unzip_table_csv,
     get_available_tables,
     get_years_and_months,
 )
 
-
-def cli_parser() -> Union[argparse.Namespace, None]:
-    description = (
+app = typer.Typer(
+    no_args_is_help=True,
+    help=(
         "A CLI utility to find and obtain data made available through "
         + "AEMO's MMS Monthly Data Archive: "
         + "http://www.nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/"
-    )
-    # create the top-level parser
-    parser = argparse.ArgumentParser(description=description)
-    subparsers = parser.add_subparsers(
-        title="subcommands",
-        description="valid subcommands",
-        help="additional help",
-        dest="subparser_name",
-    )
+    ),
+)
 
-    # create the parser for the "available-periods" command
-    subparsers.add_parser(
-        "available-periods",
-        help="Displays months and years for which data is available",
-    )
 
-    # create the parser for the "available-tables" command
-    parser_at = subparsers.add_parser(
-        "available-tables",
-        help="Displays available tables fora period (i.e. supplied month and year)",
-    )
-    parser_at.add_argument("-year", type=int, required=True, help="Data year")
-    parser_at.add_argument("-month", type=int, required=True, help="Data month")
+@app.command()
+def available_periods():
+    """
+    Displays years and the months within them for which data is available
+    """
+    print(get_years_and_months())
 
-    # create the parser for the "get-table" command
-    parser_gt = subparsers.add_parser(
-        "get-table",
-        help=(
-            "Download and unzip monthly data zip file to get data table CSV in cache. "
-            + "To see available periods, use `available-periods`, "
-            + "To see available tables for a given period, use `available-tables`"
+
+@app.command(no_args_is_help=True)
+def available_tables(year: int, month: int):
+    """
+    Displays available tables for a period (i.e. supplied month and year)
+    """
+    print(get_available_tables(year, month))
+
+
+@app.command(no_args_is_help=True)
+def get_table(
+    year: int,
+    month: int,
+    table: str,
+    cache: Annotated[
+        Path,
+        typer.Argument(
+            help="Directory to save data to. If it does not exist, it will be created"
         ),
-    )
-    parser_gt.add_argument("-year", type=int, required=True, help="Data year")
-    parser_gt.add_argument("-month", type=int, required=True, help="Data month")
-    parser_gt.add_argument("-table", type=str, required=True, help="Table name")
-    parser_gt.add_argument(
-        "-cache",
-        type=str,
-        required=True,
-        help=(
-            "Path to save data to. The directory will be"
-            + "created if it does not exist"
-        ),
-    )
-    if len(sys.argv) < 2:
-        parser.print_usage()
-        sys.exit(1)
-    else:
-        return parser.parse_args()
-
-
-def main():
-    args = cli_parser()
-    subcommand = args.subparser_name
-    if subcommand == "available-periods":
-        pprint(get_years_and_months())
-    elif subcommand == "available-tables":
-        pprint(get_available_tables(args.year, args.month))
-    elif subcommand == "get-table":
-        get_and_unzip_table_csv(args.year, args.month, args.table, args.cache)
+    ],
+):
+    """
+    Download and unzip monthly data zip file to get data table CSV in cache.
+    To see available periods, use the `available_periods` command
+    To see available tables for a given period, use the `available_tables` command
+    """
+    get_and_unzip_table_csv(year, month, table, cache)
